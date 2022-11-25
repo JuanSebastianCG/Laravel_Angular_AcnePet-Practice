a0733ad380f5234b927d080dd68cbda72d6813a9
Juan cardenas <juans.cardenasg@autonoma.edu.co>
2022-11-25 12:03:47 -0500

    modulos principales de prueba creados en el front end

--- Front_End/Acne_pets/src/app/app.module.ts
+++ Front_End/Acne_pets/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './pets/post/post.component';
+import { IndexComponent } from './pets/index/index.component';
+import { EditComponent } from './pets/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    IndexComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
