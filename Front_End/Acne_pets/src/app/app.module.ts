d3758bbb47aa43d238973e4d12e586c7cba44045
Juan cardenas <juans.cardenasg@autonoma.edu.co>
2022-11-25 22:50:40 -0500

    creacion de carpetas donde se guarda la api

--- Front_End/Acne_pets/src/app/app.module.ts
+++ Front_End/Acne_pets/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/* http inport */
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './pets/post/post.component';
@@ -18,6 +21,7 @@ import { TemplateComponent } from './index/template/template.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
