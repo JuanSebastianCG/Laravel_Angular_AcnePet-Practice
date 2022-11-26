01190ce3222529be45bec281ec158b14ea6915f5
Juan cardenas <juans.cardenasg@autonoma.edu.co>
2022-11-25 21:24:42 -0500

    login base agregada

--- Front_End/Acne_pets/src/app/app.module.ts
+++ Front_End/Acne_pets/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './pets/post/post.component';
-import { IndexComponent } from './pets/index/index.component';
 import { EditComponent } from './pets/edit/edit.component';
+import { LoginComponent } from './login/login.component';
+import { TemplateComponent } from './index/template/template.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    IndexComponent,
-    EditComponent
+    EditComponent,
+    LoginComponent,
+    TemplateComponent
   ],
   imports: [
     BrowserModule,
